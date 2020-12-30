963d9b12816092605c6bd5930563c318acc32aef
Jacob Amosa <jacobamosa@gmail.com>
2020-12-30 15:21:54 -0700

    this is my first commit yo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,39 @@
+//angular modules
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// user modules
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { HealthComponent } from './health/health.component';
+import { ThoughtsComponent } from './thoughts/thoughts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
+{
+  path: 'home',
+  component: HomeComponent
+},
+{
+  path: 'about',
+  component: AboutComponent
+},
+{
+  path: 'lessons',
+  component: LessonsComponent
+},
+{
+  path: 'health',
+  component: HealthComponent
+},
+{
+  path: 'thoughts',
+  component: ThoughtsComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LessonsComponent } from './lessons/lessons.component';
+import { ThoughtsComponent } from './thoughts/thoughts.component';
+import { HealthComponent } from './health/health.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    LessonsComponent,
+    ThoughtsComponent,
+    HealthComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
